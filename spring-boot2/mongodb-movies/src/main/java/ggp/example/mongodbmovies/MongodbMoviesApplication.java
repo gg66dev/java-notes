fa4da3fa1351013bea706f1c74029e5beccde91b
gustavo <pfeifer.g@gmail.com>
2020-07-04 13:22:02 -0400

    (springboot) project mongodb-movies

---
+++ spring-boot2/mongodb-movies/src/main/java/ggp/example/mongodbmovies/MongodbMoviesApplication.java
@@ -0,0 +1,13 @@
+package ggp.example.mongodbmovies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbMoviesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbMoviesApplication.class, args);
+	}
+
+}
---
+++ spring-boot2/mongodb-movies/src/test/java/ggp/example/mongodbmovies/MongodbMoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package ggp.example.mongodbmovies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbMoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
